24aa5c4f65ff3077b17dd75178348d265a57f691
Mo'men Mohamed <84360276+momen882001@users.noreply.github.com>
2022-10-12 13:19:20 -0700

    Home

--- greex/src/Components/Home/Home.jsx
+++ greex/src/Components/Home/Home.jsx
@@ -1,27 +1,13 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import Footer from './Footer/Footer';
+import Navbar from './NavBar/Navbar';
 
 function Home() {
   return (
     <div>
-      <Link to="/login">
-        <div className='btn btn-success'>Login</div>
-        </Link>
-      <Link to="/signup">
-        <div className='btn btn-warning'>SignUp</div>
-        </Link>
-      <Link to="/tracks">
-        <div className='btn btn-danger'>Tracks</div>
-        </Link>
-      <Link to="/awareness">
-        <div className='btn btn-info'>Awareness</div>
-        </Link>
-      <Link to="/courses">
-        <div className='btn btn-secondary'>Courses</div>
-        </Link>
-      <Link to="/aboutus">
-        <div className='btn btn-dark'>AboutUs</div>
-        </Link>
+        <Navbar/>
+        {/* <Footer/> */}
     </div>
   )
 }
--- greex/src/Components/SignUp/Signup.jsx
+++ greex/src/Components/SignUp/Signup.jsx
@@ -3,9 +3,31 @@ import '../Login/Login.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLock, faUser ,faUnlockKeyhole ,faAt} from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+// import swal from 'sweetalert';
+import Swal from 'sweetalert2'; 
 import imgRight from '../../assets/signup.svg'
 
 function Signup() {
+
+
+  const click = () => {
+      Swal.fire({
+        title: 'Video',
+        icon: 'info',
+        html:
+          '<iframe width="400" height="315" src="https://www.youtube.com/embed/d_elXY2Lcfk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        showCloseButton: true,
+        showCancelButton: true,
+        focusConfirm: false,
+        confirmButtonText:
+          'Great!',
+        confirmButtonAriaLabel: 'Thumbs up, great!',
+        cancelButtonText:
+          'close',
+        // cancelButtonAriaLabel: 'Thumbs down'
+      })
+    }
+
   return (
     <div className="login">
 
@@ -32,7 +54,7 @@ function Signup() {
                     <input type="password" placeholder="Confirm Password" required/>
                     <FontAwesomeIcon className="icon" icon={faLock} color="white" size="2x" />
                 </div>
-                <button className="submit" style={{marginTop: "10px"}}>Submit</button>
+                <button className="submit" style={{marginTop: "10px"}} onClick={click}>Submit</button>
                 <div className="other">
                   <Link to="/login">
                   <a href="">Already have an account ?</a>
