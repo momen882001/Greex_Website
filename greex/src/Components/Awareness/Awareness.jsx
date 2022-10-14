475341d448b99e6a7450251bb7bacad9cdf6255d
Mo'men Mohamed <84360276+momen882001@users.noreply.github.com>
2022-10-14 16:22:19 -0700

    awareness

--- greex/src/Components/Awareness/Awareness.jsx
+++ greex/src/Components/Awareness/Awareness.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import './awareness.css'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Link } from "react-router-dom";
+import imgLeft from '../../assets/34474031.png'
+import Navbar from '../Home/NavBar/Navbar';
+// import Video2 from './Video/Video2';
+
+
 
 function Awareness() {
   return (
     <div>
-      Awareness
+        <Navbar/>
+      <div className="container">
+        <section className="side" style={{padding:"0", margin:"0"}}>
+          <img src= {imgLeft} alt="" size="2x" style={{padding:"0", margin:"0"}}/>
+        </section>
+          <Row className='row'>
+            <Col className="awr col-3" xs={12} sm={6} md={6} lg={3}>Green jobs</Col>
+            <Col className="awr col-3" xs={12} sm={6} md={6} lg={3}>Green entrepreneurship</Col>
+            <Col className="awr col-3" xs={12} sm={6} md={6} lg={3}>Green building materials</Col>
+            <Col className="awr col-3" xs={12} sm={6} md={6} lg={3}>Green architecture</Col>
+            <Col className="awr col-3" xs={12} sm={6} md={6} lg={3}>Green energy</Col>
+            <Col className="awr col-3" xs={12} sm={6} md={6} lg={3}>Green farming</Col>
+            <Col className="awr col-3" xs={12} sm={6} md={6} lg={3}>Green manufacturing</Col>
+            <Col className="awr col-3" xs={12} sm={6} md={6} lg={3}>Waste recycling</Col>
+            <Col className="awr col-3" xs={12} sm={6} md={6} lg={3}>Water managements</Col>
+            <Col className="awr col-3" xs={12} sm={6} md={6} lg={3}>Environmental Impact Assessment</Col>
+          </Row>
+    </div>
     </div>
   )
 }
 
 export default Awareness
+
