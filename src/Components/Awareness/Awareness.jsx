825baccecfba9312ebe1740049c18f4e0c1cf925
Abdelrhman Yasser <93376951+Abdelrhman012@users.noreply.github.com>
2022-11-03 01:55:41 +0200

    awareness responsive

--- src/Components/Awareness/Awareness.jsx
+++ src/Components/Awareness/Awareness.jsx
@@ -12,22 +12,22 @@ function Awareness() {
     <div>
         <Navbar/>
         <Video2/>
+        <h1>Our Tracks</h1>
       <div className="container">
-        <h2>Our Tracks</h2>
         <section className="side" style={{padding:"0", margin:"0"}}>
           <img src= {imgLeft} alt="" size="2x" style={{padding:"0", margin:"0"}}/>
         </section>
           <Row className='row'>
-            <Col className="awr col-3" xs={12} sm={6} md={6} lg={3}>Green jobs</Col>
-            <Col className="awr col-3" xs={12} sm={6} md={6} lg={3}>Green entrepreneurship</Col>
-            <Col className="awr col-3" xs={12} sm={6} md={6} lg={3}>Green building materials</Col>
-            <Col className="awr col-3" xs={12} sm={6} md={6} lg={3}>Green architecture</Col>
-            <Col className="awr col-3" xs={12} sm={6} md={6} lg={3}>Green energy</Col>
-            <Col className="awr col-3" xs={12} sm={6} md={6} lg={3}>Green farming</Col>
-            <Col className="awr col-3" xs={12} sm={6} md={6} lg={3}>Green manufacturing</Col>
-            <Col className="awr col-3" xs={12} sm={6} md={6} lg={3}>Waste recycling</Col>
-            <Col className="awr col-3" xs={12} sm={6} md={6} lg={3}>Water managements</Col>
-            <Col className="awr col-3" xs={12} sm={6} md={6} lg={3}>Environmental Impact Assessment</Col>
+            <Col className="awr col-3" xs={12} sm={12} md={4} lg={3}>Green jobs</Col>
+            <Col className="awr col-3" xs={12} sm={12} md={4} lg={3}>Green entrepreneurship</Col>
+            <Col className="awr col-3" xs={12} sm={12} md={4} lg={3}>Green building materials</Col>
+            <Col className="awr col-3" xs={12} sm={12} md={4} lg={3}>Green architecture</Col>
+            <Col className="awr col-3" xs={12} sm={12} md={4} lg={3}>Green energy</Col>
+            <Col className="awr col-3" xs={12} sm={12} md={4} lg={3}>Green farming</Col>
+            <Col className="awr col-3" xs={12} sm={12} md={4} lg={3}>Green manufacturing</Col>
+            <Col className="awr col-3" xs={12} sm={12} md={4} lg={3}>Waste recycling</Col>
+            <Col className="awr col-3" xs={12} sm={12} md={4} lg={3}>Water managements</Col>
+            <Col className="awr col-3" xs={12} sm={12} md={4} lg={3}>Environmental Impact Assessment</Col>
           </Row>
     </div>
     </div>
